6c3bb9639368e7b8d1d68cf53d4cc54a69041159
Jacek Kosinski <kashiash@gmail.com>
2023-10-03 11:15:42 +0200

    Initial Commit

---
+++ BookDiaryX/BookDiaryXApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookDiaryXApp.swift
+//  BookDiaryX
+//
+//  Created by Jacek Kosinski U on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookDiaryXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
