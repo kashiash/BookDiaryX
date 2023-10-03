9b6b19392eb06cf3fec409e3cc8b8a0c719cf27d
Jacek Kosinski <kashiash@gmail.com>
2023-10-03 12:25:38 +0200

    .

---
+++ BookDiaryX/NotesListView.swift
@@ -0,0 +1,18 @@
+//
+//  NotesListView.swift
+//  BookDiaryX
+//
+//  Created by Jacek Kosinski U on 03/10/2023.
+//
+
+import SwiftUI
+
+struct NotesListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotesListView()
+}
